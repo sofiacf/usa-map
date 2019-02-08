c8fedc6bac2dad55851f6d9c4c195ee497c210e4
sofiacf <46513507+sofiacf@users.noreply.github.com>
2019-02-08 11:35:26 -0500

    Further updated courier list url

--- scripts/usamap.js
+++ scripts/usamap.js
@@ -100,7 +100,7 @@ function styleFeature(feature) {
 	};
 }
 var script = document.createElement('script');
-script.src = 'https://smcf.io/scripts/couriers.js';
+script.src = '/scripts/couriers.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 window.eqfeed_callback = function(data) {
 	map.data.addGeoJson(data);
