619f7abf2a8410fe73de6cf986a3b442855cf142
sofiacf <sofia@smcf.io>
2019-02-08 17:49:54 -0500

    New panel displays distance, quote

--- scripts/usamap.js
+++ scripts/usamap.js
@@ -1,10 +1,9 @@
 //Add infowindow for route, display distance
 //Save directions to server, display multiple routes using array of directionsRenderers
-//Add info windows for couriers onclick
-//
 
 var map;
 var infowindow;
+document.getElementById('info-panel').style.display = 'none';
 function initMap() {
 	var directionsService = new google.maps.DirectionsService();
 	var directionsDisplay = new google.maps.DirectionsRenderer();
@@ -19,16 +18,21 @@ function initMap() {
 	var originInput = document.getElementById('origin-input');
 	var destinationInput = document.getElementById('destination-input');
 	var jobTypeSelector = document.getElementById('job-type-selector');
+	var infoPanel = document.getElementById('info-panel');
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
   	map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
   	map.controls[google.maps.ControlPosition.TOP_LEFT].push(jobTypeSelector);
+  	map.controls[google.maps.ControlPosition.RIGHT].push(infoPanel);
 	directionsDisplay.setMap(map);
 	map.data.setStyle(styleFeature);
 	map.data.addListener('click', function(event) {
 		var f = event.feature;
 		var name = f.getProperty('Name');
-		var type = f.getProperty('type');
-		infowindow.setContent(name);
+		var description = f.getProperty('description');
+		if (!description) {
+			description = "Please add details";
+		}
+		infowindow.setContent(`<div><p>${name}</p><p>${description}</p></div>`);
 		infowindow.setPosition(f.getGeometry().get());
 		infowindow.setOptions({
 			pixelOffset: new google.maps.Size(0, -30)
@@ -41,7 +45,6 @@ function initMap() {
 	document.getElementById('origin-input').addEventListener('change', onChangeHandler);
 	document.getElementById('destination-input').addEventListener('change', onChangeHandler);
 }
-
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   	var pickup = document.getElementById('origin-input').value;
   	var delivery = document.getElementById('destination-input').value;
@@ -52,20 +55,14 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   	};
   	directionsService.route(request, function(result, status) {
   		if (status == 'OK') {
+  			document.getElementById('info-panel').style.display = "inline";
   			directionsDisplay.setDirections(result);
-  			console.log(result.routes[0].legs[0].distance.text);
+  			var distance = parseInt(result.routes[0].legs[0].distance.text);
+  			document.getElementById('mileage').innerHTML = distance + " mi";
+  			document.getElementById('quote').innerHTML = 45 + (distance * 2.25);
+  			
   		}
   	})
-}
-function createMarker(place) {
-	var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location
-	});
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);
-	});
 }
 function styleFeature(feature) {
 	var type = feature.getProperty('type');
@@ -81,14 +78,12 @@ function styleFeature(feature) {
 		}
 	};
 }
-
 var script = document.createElement('script');
-script.src = 'scripts/couriers.js';
+script.src = 'https://smcf.io/scripts/couriers.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 window.eqfeed_callback = function(data) {
 	map.data.addGeoJson(data);
 }
-
 var mapStyle = [{
 	'featureType': 'all',
 	'elementType': 'all',
