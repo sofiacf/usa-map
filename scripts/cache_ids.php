66a19027ce02bf5de6f63ce1311109964a439b71
sofiacf <sofia@smcf.io>
2019-03-14 13:39:40 -0400

    Fixed error in special character replacement

--- scripts/cache_ids.php
+++ scripts/cache_ids.php
@@ -5,8 +5,8 @@ function parseToXML($htmlStr)
 {
 $xmlStr=str_replace('<','&lt;',$htmlStr);
 $xmlStr=str_replace('>','&gt;',$xmlStr);
-$xmlStr=str_replace('"','&apos;',$xmlStr);
-$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&apos;',$xmlStr);
 $xmlStr=str_replace("&",'&amp;',$xmlStr);
 return $xmlStr;
 }
